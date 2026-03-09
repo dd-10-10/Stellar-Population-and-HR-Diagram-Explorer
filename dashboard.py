628ff2bf5c89026e42bb94a0899a2be62b0e4488
dd-10-10 <dheeraj.deshpande@students.iiserpune.ac.in>
2026-03-09 13:34:40 +0000

    Added colour scheme selector and colour scheme options, and reduced size of scatter datapoints

--- dashboard.py
+++ dashboard.py
@@ -8,9 +8,11 @@ def dashboard(df):
 
     x_ax= st.sidebar.selectbox("X-axis:", df.columns)
     y_ax= st.sidebar.selectbox("Y-axis:", df.columns)
-    clr= st.sidebar.selectbox("Colour based on:", df.columns)
+    clr_ax= st.sidebar.selectbox("Colour based on:", df.columns)
+    clr_scl= st.sidebar.selectbox("Colour scheme:", ["Blues", "Greys", "Plasma", "RdYlBu", "Reds"])
 
-    fig= px.scatter(df, x=x_ax, y=y_ax, color= clr, color_continuous_scale= "rdylbu")
+    fig= px.scatter(df, x=x_ax, y=y_ax, color= clr_ax, color_continuous_scale= clr_scl)
+    fig.update_traces(marker={'size': 1})
 
     st.write(fig)
     st.write(df)
