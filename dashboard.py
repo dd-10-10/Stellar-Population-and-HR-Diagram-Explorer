6254660188857a7b574b071b00939fdb72dd3145
dd-10-10 <dheeraj.deshpande@students.iiserpune.ac.in>
2026-03-19 06:53:37 +0000

    Created a combined data file to pass to dashboard, to allow for a distance slider

--- dashboard.py
+++ dashboard.py
@@ -3,37 +3,43 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-def dashboard(df1, df2):
+def dashboard(df):
+    '''
+    Function to generate the diagram and dashboard from data
+    '''
     st.title("Hertzsprung-Russell Diagram Explorer")
 
-    data= st.selectbox("Dataset:", ["Near-Earth", "Far-Earth"])
-    if data== "Near-Earth":
-        df= df1
-    else:
-        df= df2
+    #Selecting data based on distance from Earth
+    vals= st.slider("Select range of distance from Earth:", df["Distance"].min(), df["Distance"].max(), (1.0, 100.0))
+    
+    #Selecting type of diagram; MAY REMOVE IN FUTURE (Theoretical HR diagram cannot be made with only observational data)
 
-    diag_type= st.sidebar.selectbox("Diagram type:", ["Observational", "Theoretical"])
+    #diag_type= st.sidebar.selectbox("Diagram type:", ["Observational", "Theoretical"])
+    diag_type= "Observational"
     if diag_type== "Observational":
         x_ax= "Color index"
         y_ax= "Absolute magnitude"
     else:
         x_ax= "Log effective temperature"
         y_ax= "Log luminosity"
-
+    
+    #Color
     clr_ax= "Color index"
-    clr_scl= st.sidebar.selectbox("Colour scheme:", ["Blues", "Greys", "Plasma", "RdYlBu", "Reds"])
+    clr_scl= st.selectbox("Colour scheme:", ["Blues", "Greys", "Plasma", "RdYlBu", "Reds"])
 
-    fig= px.scatter(df, x=x_ax, y=y_ax, color= clr_ax, color_continuous_scale= clr_scl+"_r", range_color=[-0.5, 2])
+    #Plotting the data
+    fig= px.scatter(df[(df["Distance"]>= vals[0]) & (df["Distance"]<= vals[1])], x=x_ax, y=y_ax, color= clr_ax, color_continuous_scale= clr_scl+"_r", range_color=[-0.5, 2])
     fig.update_traces(marker={'size': 1})
     if diag_type== "Observational":
         fig.update_yaxes(autorange="reversed")
     else:
         fig.update_xaxes(autorange="reversed")
 
+    #Displaying the plot (and, for now, the data)
     st.write(fig)
     st.write(df)
 
+#Execution
 if __name__== "__main__":
-    df1 = pd.read_csv("near_100k_clean_calc.csv")
-    df2 = pd.read_csv("far_100k_clean_calc.csv")
-    dashboard(df1, df2)
+    df = pd.read_csv("stars_clean_calc.csv")
+    dashboard(df)
--- file_setup.py
+++ file_setup.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
 
+#---- Calculations and other functions; see bottom for actual setup, cleaning, and filtering ----
+
 def new_names(df):
+    '''
+    Function to rename the columns to better appearing names
+    '''
     n_dict = {"phot_g_mean_mag" : "Apparent G magnitude",
              "bp_rp" : "Color index",
              "parallax" : "Parallax",
@@ -13,25 +18,40 @@ def new_names(df):
     df.rename(columns = n_dict, inplace = True)
 
 def distance(df):
+    '''
+    Function to calculate distance from parallax
+    '''
     d = 1000/df["Parallax"]
     return d
 
 def abs_vis_mag(df):
+    '''
+    Function to calculate absolute visual magnitude
+    '''
     g, g_ext, p = df["Apparent G magnitude"], df["Extinction in G band"], df["Parallax"] 
     v = g + 0.0176 + 0.00686 * df["Color index"] + 0.1732 * ((df["Color index"])**2)
     mv= v - (1.1 * g_ext) + (5 * np.log10(p)) - 10
     return mv
 
 def abs_mag(df):
+    '''
+    Function to calculate absolute g band magnitude
+    '''
     g, g_ext, p = df["Apparent G magnitude"], df["Extinction in G band"], df["Parallax"]
     mg= g - g_ext + (5 * np.log10(p)) - 10
     return mg
 
 def log_lum(df):
+    '''
+    Function to calculate luminosity (log_10)
+    '''
     l = 0.4 * (4.66 - abs_mag(df))
     return l
 
 def spectral_class(row):
+    '''
+    Function to identify spectral class from temperature
+    '''
     if row["Effective temperature"] > 30000:
         return 'O'
     elif row["Effective temperature"] > 10000:
@@ -48,31 +68,33 @@ def spectral_class(row):
         return 'M'
 
 def spec(df):
+    '''
+    Function to vectorise the spectral class identifier
+    '''
     return df.apply(spectral_class, axis = 1)
 
+#---- Actual file setup ----
+
 if __name__ == "__main__":
+    
+    #Creating one combined dataframe
     df1 = pd.read_csv("near_100k.csv")
-    df1.dropna(axis = 0, inplace = True)
-    new_names(df1)
-    df1["Distance"] = distance(df1)
-    df1["Absolute visual magnitude"] = abs_vis_mag(df1)
-    df1["Absolute magnitude"] = abs_mag(df1)
-    df1["Log luminosity"] = log_lum(df1)
-    df1["Log effective temperature"] = np.log10(df1["Effective temperature"])
-    df1["Log surface gravity"] = np.log10(df1["Surface gravity"])
-    df1["Spectral class"] = spec(df1)
-
-    df1.to_csv("near_100k_clean_calc.csv", index= False)
-
     df2 = pd.read_csv("far_100k.csv")
-    df2.dropna(axis = 0, inplace = True)
-    new_names(df2)
-    df2= df2[df2["Parallax"] > 0]
-    df2["Distance"] = distance(df2)
-    df2["Absolute visual magnitude"] = abs_vis_mag(df2)
-    df2["Absolute magnitude"] = abs_mag(df2)
-    df2["Log luminosity"] = log_lum(df2)
-    df2["Log effective temperature"] = np.log10(df2["Effective temperature"])
-    df2["Log surface gravity"] = np.log10(df2["Surface gravity"])
-    df2["Spectral class"] = spec(df2)
-    df2.to_csv("far_100k_clean_calc.csv", index= False)
+    df2= df2[df2["parallax"] > 0]
+    df= pd.concat((df1, df2))
+
+    #Calculating and adding new columns
+    df.dropna(axis = 0, inplace = True)
+    new_names(df)
+    df["Distance"] = distance(df)
+    df["Absolute visual magnitude"] = abs_vis_mag(df)
+    df["Absolute magnitude"] = abs_mag(df)
+    df["Log luminosity"] = log_lum(df)
+    df["Log effective temperature"] = np.log10(df["Effective temperature"])
+    df["Log surface gravity"] = np.log10(df["Surface gravity"])
+    df["Spectral class"] = spec(df)
+
+    #Quality filters
+
+    #Saving
+    df.to_csv("stars_clean_calc.csv", index= False)
