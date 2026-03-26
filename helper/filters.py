6bf16b7ee18b819cc49edf7cb5f8b821098ba1b1
Mokshaavasava35 <moksha.20231148@students.iiserpune.ac.in>
2026-03-27 00:19:35 +0530

    Update filters.py

--- helper/filters.py
+++ helper/filters.py
@@ -15,30 +15,24 @@ def hard_filters(df):
     # Post calculations Quality filters   
     df = df[df["Extinction in G band"] >= 0]
     df = df[(df["Log luminosity"] >= -6) & (df["Log luminosity"] <= 6)]
-    
-    # Statistical filters (IQR method to remove telescope sensor noise)
+
+ """
+ More error columns are needed like phot_g_mean_mag_error , bp_rp_error , teff_gspphot_error , to apply stat filters
+ """
+
+    # Statistical filter (IQR method to remove telescope sensor noise)
     def statistical_error_cut(df, column_name):
-  
-    # Find the 25th percentile (Q1) and 75th percentile (Q3)
         Q1 = df[column_name].quantile(0.25)
         Q3 = df[column_name].quantile(0.75)
-    
-    # Calculate the size of the "normal" middle section (IQR)
         IQR = Q3 - Q1
-    
-    # Build the upper boundary fence (Standard formula is Q3 + 1.5 * IQR)
         upper_bound = Q3 + 1.5 * IQR
-    
-    # Filter the dataframe to only keep the reliable, normal errors
         df_clean = df[df[column_name] <= upper_bound]
     
         return df_clean
+# Statistical filter on parallax error
     if "Parallax error" in df.columns:
         df = statistical_error_cut(df, "Parallax error")
     
     return df
- """
- More error columns are needed like phot_g_mean_mag_error , bp_rp_error , teff_gspphot_error , to apply stat filters
- """
 
 
