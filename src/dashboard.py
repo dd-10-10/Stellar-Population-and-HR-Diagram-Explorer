2bb2ee5aa27b6a454aa65f7a36922c128b70ca6e
dd-10-10 <dheeraj.deshpande@students.iiserpune.ac.in>
2026-03-31 04:53:31 +0000

    Added sliders that will disappear if selected as axis

--- src/dashboard.py
+++ src/dashboard.py
@@ -34,32 +34,15 @@ def dashboard(df):
             gmag_vals= num_slider(label= "Apparent G magnitude", min_val= df["Apparent G magnitude"].min(), max_val= df["Apparent G magnitude"].max(),
                                 step= step_size(df["Apparent G magnitude"].min(), df["Apparent G magnitude"].max()), sl_key= "gmag_sl")
             df= df[(df["Apparent G magnitude"]>= gmag_vals[0]) & (df["Apparent G magnitude"]<= gmag_vals[1])]
-            # TODO: Show only one or the other, based on which one is the currently selected axis
-            clr_vals= num_slider(label= "Color index", min_val= df["Color index"].min(), max_val= df["Color index"].max(),
-                                step= step_size(df["Color index"].min(), df["Color index"].max()), sl_key= "clr_sl")
-            df= df[(df["Color index"]>= clr_vals[0]) & (df["Color index"]<= clr_vals[1])]
-            
-            temp_vals= num_slider(label= "Effective temperature", min_val= df["Effective temperature"].min(), max_val= df["Effective temperature"].max(),
-                                step= step_size(df["Effective temperature"].min(), df["Effective temperature"].max()), sl_key= "temp_sl")
-            df= df[(df["Effective temperature"]>= temp_vals[0]) & (df["Effective temperature"]<= temp_vals[1])]
-            #----------
             
             grav_vals= num_slider(label= "Surface gravity", min_val= df["Surface gravity"].min(), max_val= df["Surface gravity"].max(),
                                 step= step_size(df["Surface gravity"].min(), df["Surface gravity"].max()), sl_key= "grav_sl")
             df= df[(df["Surface gravity"]>= grav_vals[0]) & (df["Surface gravity"]<= grav_vals[1])]
-            # TODO: Show only one or the other, based on which one is the currently selected axis
-            abs_vals= num_slider(label= "Absolute magnitude", min_val= df["Absolute magnitude"].min(), max_val= df["Absolute magnitude"].max(),
-                                step= step_size(df["Absolute magnitude"].min(), df["Absolute magnitude"].max()), sl_key= "abs_sl")
-            df= df[(df["Absolute magnitude"]>= abs_vals[0]) & (df["Absolute magnitude"]<= abs_vals[1])]
-            
-            lum_vals= num_slider(label= "Luminosity (log)", min_val= df["Log luminosity"].min(), max_val= df["Log luminosity"].max(),
-                                step= step_size(df["Log luminosity"].min(), df["Log luminosity"].max()), sl_key= "lum_sl")
-            df= df[(df["Log luminosity"]>= lum_vals[0]) & (df["Log luminosity"]<= lum_vals[1])]
-            #----------
+
             # Filters
             #df= hard_filter(df, ["Apparent G magnitude", "Color index", "Effective temperature", "Log luminosity"])
             #df= del_outliers(df, ["Apparent G magnitude", "Color index", "Effective temperature", "Log luminosity"])
-        
+
     # HR Diagram
     with out_col:
         st.subheader("HR Diagram")
@@ -70,7 +53,30 @@ def dashboard(df):
             x_ax= st.selectbox("X-axis:", ["Color index", "Log effective temperature"])
         with ycol:
             y_ax= st.selectbox("Y-axis:", ["Absolute magnitude", "Log luminosity"])
-
+    
+    with filter_col:
+        with subcont:
+            # Displaying different sliders based on chosen x axis
+            if x_ax== "Color index":
+                temp_vals= num_slider(label= "Effective temperature", min_val= df["Effective temperature"].min(), max_val= df["Effective temperature"].max(),
+                                      step= step_size(df["Effective temperature"].min(), df["Effective temperature"].max()), sl_key= "temp_sl")
+                df= df[(df["Effective temperature"]>= temp_vals[0]) & (df["Effective temperature"]<= temp_vals[1])]
+            else:
+                clr_vals= num_slider(label= "Color index", min_val= df["Color index"].min(), max_val= df["Color index"].max(),
+                                     step= step_size(df["Color index"].min(), df["Color index"].max()), sl_key= "clr_sl")
+                df= df[(df["Color index"]>= clr_vals[0]) & (df["Color index"]<= clr_vals[1])]
+            
+            # Displaying different sliders based on chosen y axis
+            if y_ax== "Absolute magnitude":
+                lum_vals= num_slider(label= "Luminosity (log)", min_val= df["Log luminosity"].min(), max_val= df["Log luminosity"].max(),
+                                     step= step_size(df["Log luminosity"].min(), df["Log luminosity"].max()), sl_key= "lum_sl")
+                df= df[(df["Log luminosity"]>= lum_vals[0]) & (df["Log luminosity"]<= lum_vals[1])]
+            else:
+                abs_vals= num_slider(label= "Absolute magnitude", min_val= df["Absolute magnitude"].min(), max_val= df["Absolute magnitude"].max(),
+                                     step= step_size(df["Absolute magnitude"].min(), df["Absolute magnitude"].max()), sl_key= "abs_sl")
+                df= df[(df["Absolute magnitude"]>= abs_vals[0]) & (df["Absolute magnitude"]<= abs_vals[1])]
+    
+    with out_col:
         # Color
         clr_ax= "Color index"
         clr_scl= st.selectbox("Colour scheme:", ["Blues", "Greys", "Plasma", "RdYlBu", "Reds"], index= 3)
@@ -86,11 +92,13 @@ def dashboard(df):
         
         if x_ax== "Color index":
             fig.update_xaxes(range= [-0.5, 5])
-        elif x_ax== "Log effective temperature":
+        else:
             fig.update_xaxes(range= [np.log10(30000), np.log10(1000)])
 
         # Displaying the plot
         st.write(fig)
+
+
 
 # Execution
 if __name__== "__main__":
